73b4384e0f256dd340faae8f2cd7fb78a48f6479
norasotanext <minhbn@sotanext.com>
2022-05-05 15:27:40 +0700

    feat: add grpc.go

---
+++ grpc.go
@@ -0,0 +1,41 @@
+package erpc
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/protobuf/types/known/timestamppb"
+)
+
+const (
+	authPermissionClientKey = "authPermissionClientKey"
+	authSessionClientKey    = "authSessionClientKey"
+	componentClientKey      = "componentClientKey"
+)
+
+// GetConnection return GRPC Connection of input url
+// It will create connection if it is closed and return connection if connected
+func GetConnection(hostURL string, timeout time.Duration) (*grpc.ClientConn, error) {
+	ctx, cancelFunc := context.WithTimeout(context.Background(), timeout)
+	defer cancelFunc()
+	clientConn, err := grpc.DialContext(
+		ctx,
+		hostURL,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return clientConn, nil
+}
+
+// Time2TimePb converts Golang *time.Time to *timestamppb.Timestamp to use in gRPC response
+func Time2TimePb(t *time.Time) *timestamppb.Timestamp {
+	if t != nil {
+		return timestamppb.New(*t)
+	}
+	return nil
+}
